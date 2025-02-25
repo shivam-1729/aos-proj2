48de7bdce53b21f6461f087ccb5facdd4404d089
Gaurav Dadlaney <gdadlaney3@gatech.edu>
2025-02-24 19:40:36 -0500

    Complete Sense-reversing barrier using OpenMP -> gtmp1.c

--- omp/gtmp1.c
+++ omp/gtmp1.c
@@ -1,16 +1,49 @@
+#include <stdio.h>
 #include <omp.h>
+#include <stdbool.h>
 #include "gtmp.h"
 
+bool DEBUG = false;
+#define debug_print(fmt, ...) do { if (DEBUG) printf(fmt, __VA_ARGS__); } while (0) 
+// Not sure of the performance hit of these if statements, but it'll definitely be lower than printf
+
+
+int threads_to_wait_for;
+int thread_count;
+bool sense = false;
 // todo(Gaurav): Sense Reversing Barrier
 void gtmp_init(int num_threads){
+    // omp_set_num_threads(num_threads);
+    thread_count = num_threads;
+    threads_to_wait_for = thread_count;
 
+    printf("Init completed\n");
 }
 
 void gtmp_barrier(){
+    bool my_sense = !sense;
 
+    int thread_id = omp_get_thread_num();
+    
+    // #pragma omp atomic
+    //     threads_to_wait_for--;
+    threads_to_wait_for = __sync_fetch_and_sub(&threads_to_wait_for, 1);
+    threads_to_wait_for--;
+
+    debug_print("[%d] threads_to_wait_for is now: %d\n", thread_id, threads_to_wait_for);
+
+    if (0 == threads_to_wait_for) {
+        debug_print("Thread: %d has now reset the barrier\n", thread_id);
+        threads_to_wait_for = thread_count;
+        sense = my_sense;
+    } else {
+        // printf("Thread: %d will now spin\n", thread_id);
+        while(my_sense != sense);
+    }
 }
 
 void gtmp_finalize(){
-
+    // No action required to clean up after using OpenMP
+    printf("Finalize completed\n");
 }
 
