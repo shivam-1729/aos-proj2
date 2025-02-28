40c916b60d02e62a6861b6fcac1816f470c5fea4
Gaurav Dadlaney <gdadlaney3@gatech.edu>
2025-02-27 23:59:12 -0500

    Clean up log lines

--- mpi/gtmpi1.c
+++ mpi/gtmpi1.c
@@ -23,6 +23,7 @@ int process_cnt;
 void gtmpi_init(int num_processes) {
     // node = malloc(num_processes * sizeof(TreeNode));
     process_cnt = num_processes;
+    // printf("Init completed\n");  // This is being called by every process
 }
 
 void wake_up(int my_id) {
@@ -33,16 +34,17 @@ void wake_up(int my_id) {
 
     if (my_id != 0) {
         MPI_Recv(&my_msg, 1, MPI_INT, parent_index, tag, MPI_COMM_WORLD, &mpi_result);
-        printf("[%d] move to next barrier signal received from %d\n", my_id, parent_index);
+        // printf("[%d] move to next barrier signal received from %d\n", my_id, parent_index);
+    } else {
+        printf("Wake-up step has now started\n");
     }
 
     if (is_parent) {
         for (int j = 1; j <= WAKE_UP_N_ARY && (WAKE_UP_N_ARY * my_id + j) < process_cnt; j++) {
             int child_index = (WAKE_UP_N_ARY * my_id + j);
-            // printf("[%d] Waiting for message from %d\n", my_id, child_index);
             my_msg = 1;
             MPI_Send(&my_msg, 1, MPI_INT, child_index, tag, MPI_COMM_WORLD);       // relying on blocking nature
-            printf("[%d] Wake-up signal sent to %d\n", my_id, child_index);
+            // printf("[%d] Wake-up signal sent to %d\n", my_id, child_index);
         }
     }
 }
@@ -54,14 +56,14 @@ void arrival_tree(int my_id) {
     //     MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     // }
 
-    TreeNode node;
+    // TreeNode node;
     // for (int j = 1; j <= ARRIVAL_N_ARY; j++) {
     //     node.have_child[j] = ((ARRIVAL_N_ARY*my_id + j) < process_cnt);
     //     node.child_not_ready[j] = node.have_child[j];       // A non-existant child is ready. Existing children are not ready.
     // }
     // bool is_parent = node.have_child[0];
     bool is_parent = (ARRIVAL_N_ARY*my_id + 1) < process_cnt;
-    bool all_children_not_ready = true;
+    // bool all_children_not_ready = true;
 
     int tag = 1;
     MPI_Status mpi_result;
@@ -70,9 +72,8 @@ void arrival_tree(int my_id) {
     if (is_parent) {
         for (int j = 1; j <= ARRIVAL_N_ARY && (ARRIVAL_N_ARY * my_id + j) < process_cnt; j++) {
             int child_index = (ARRIVAL_N_ARY * my_id + j);
-            // printf("[%d] Waiting for message from %d\n", my_id, child_index);
             MPI_Recv(&my_msg, 1, MPI_INT, child_index, tag, MPI_COMM_WORLD, &mpi_result);       // relying on blocking nature
-            printf("[%d] %d is now ready\n", my_id, child_index);
+            // printf("[%d] %d is now ready\n", my_id, child_index);
         }
     }
     
@@ -80,7 +81,9 @@ void arrival_tree(int my_id) {
     int parent_index = (my_id - 1) / ARRIVAL_N_ARY;      // right shift by 2 would already be optimized by a good compiler.
     if (my_id != parent_index) {
         MPI_Send(&my_msg, 1, MPI_INT, parent_index, tag, MPI_COMM_WORLD);
-        printf("[%d] Message sent to %d\n", my_id, parent_index);
+        // printf("[%d] Message sent to %d\n", my_id, parent_index);
+    } else {
+        printf("Arrival step has now completed\n");
     }
     
     // if (my_id != parent) {
@@ -89,7 +92,7 @@ void arrival_tree(int my_id) {
 }
 
 void gtmpi_barrier(){
-    bool sense = true;
+    // bool sense = true;
     int my_id;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
     
@@ -106,7 +109,8 @@ void gtmpi_barrier(){
 }
 
 void gtmpi_finalize(){
-
+    // No action required to clean up after using MPI
+    // printf("Finalize completed\n"); // This is being called by every process
 }
 
 // type treenode record
