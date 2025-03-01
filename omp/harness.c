c89f0363f80a878d15127164251e8dfefd70814e
Gaurav Dadlaney <gdadlaney3@gatech.edu>
2025-03-01 16:38:31 -0500

    Fixed experiments & now made harness.c print max time diff for each round

--- omp/harness.c
+++ omp/harness.c
@@ -3,11 +3,12 @@
 #include <omp.h>
 #include "gtmp.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  int num_threads, num_iter=10;
+  int num_threads, num_rounds = 10;
 
-  if (argc < 2){
+  if (argc < 2)
+  {
     fprintf(stderr, "Usage: ./harness [NUM_THREADS]\n");
     exit(EXIT_FAILURE);
   }
@@ -18,18 +19,43 @@ int main(int argc, char** argv)
     printf("Warning: dynamic adjustment of threads has been set\n");
 
   omp_set_num_threads(num_threads);
-  
+
   gtmp_init(num_threads);
 
-#pragma omp parallel shared(num_threads)
-   {
-     int i;
-     for(i = 0; i < num_iter; i++){
-       gtmp_barrier();
-     }
-   }
+  double all_latencies[num_rounds][num_threads];
 
-   gtmp_finalize();
+#pragma omp parallel shared(num_threads, all_latencies)
+  {
+    double start_time;
+    double max_latency;
+    int thread_id = omp_get_thread_num();
 
-   return 0;
+    for (int i = 0; i < num_rounds; i++)
+    {
+      start_time = omp_get_wtime();     // todo: check if walltime should be 
+      gtmp_barrier();
+      all_latencies[i][thread_id] = omp_get_wtime() - start_time;     // Can this array layout cause cache coherence problems?
+    }
+
+    // Calculate the maximum latency for each round only on the root process
+    if (0 == thread_id)
+    {
+      for (int k = 0; k < num_rounds; k++)
+      {
+        max_latency = all_latencies[k][0];
+        for (int i = 1; i < num_threads; i++)
+        {
+          if (all_latencies[k][i] > max_latency)
+          {
+            max_latency = all_latencies[k][i];
+          }
+        }
+        printf("[%d] Max latency for round %d: %lf\n", thread_id, k, max_latency);    // todo: can also do max_end_time - min_end_time
+      }
+    }
+  }
+
+  gtmp_finalize();
+
+  return 0;
 }
--- omp/run_experiments.sh
+++ omp/run_experiments.sh
@@ -1,8 +1,8 @@
-sbatch threads.sbatch --job-name mp1_1 --cpus-per-task 1 ./mp1 1
-sbatch threads.sbatch --job-name mp1_2 --cpus-per-task 2 ./mp1 2
-sbatch threads.sbatch --job-name mp1_3 --cpus-per-task 3 ./mp1 3
-sbatch threads.sbatch --job-name mp1_4 --cpus-per-task 4 ./mp1 4
-sbatch threads.sbatch --job-name mp1_5 --cpus-per-task 5 ./mp1 5
-sbatch threads.sbatch --job-name mp1_6 --cpus-per-task 6 ./mp1 6
-sbatch threads.sbatch --job-name mp1_7 --cpus-per-task 7 ./mp1 7
-sbatch threads.sbatch --job-name mp1_8 --cpus-per-task 8 ./mp1 8
+sbatch --job-name=mp1_1 --cpus-per-task=1 threads.sbatch ./mp1 1
+sbatch --job-name=mp1_2 --cpus-per-task=2 threads.sbatch ./mp1 2
+sbatch --job-name=mp1_3 --cpus-per-task=3 threads.sbatch ./mp1 3
+sbatch --job-name=mp1_4 --cpus-per-task=4 threads.sbatch ./mp1 4
+sbatch --job-name=mp1_5 --cpus-per-task=5 threads.sbatch ./mp1 5
+sbatch --job-name=mp1_6 --cpus-per-task=6 threads.sbatch ./mp1 6
+sbatch --job-name=mp1_7 --cpus-per-task=7 threads.sbatch ./mp1 7
+sbatch --job-name=mp1_8 --cpus-per-task=8 threads.sbatch ./mp1 8
