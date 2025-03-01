0f991afcfff2e5635648aa4d992e00510917df3c
Gaurav Dadlaney <gdadlaney3@gatech.edu>
2025-03-01 12:23:21 -0500

    Testing of experiment setup for omp

---
+++ omp/run_experiments.sh
@@ -0,0 +1,8 @@
+sbatch threads.sbatch --job-name mp1_1 --cpus-per-task 1 ./mp1 1
+sbatch threads.sbatch --job-name mp1_2 --cpus-per-task 2 ./mp1 2
+sbatch threads.sbatch --job-name mp1_3 --cpus-per-task 3 ./mp1 3
+sbatch threads.sbatch --job-name mp1_4 --cpus-per-task 4 ./mp1 4
+sbatch threads.sbatch --job-name mp1_5 --cpus-per-task 5 ./mp1 5
+sbatch threads.sbatch --job-name mp1_6 --cpus-per-task 6 ./mp1 6
+sbatch threads.sbatch --job-name mp1_7 --cpus-per-task 7 ./mp1 7
+sbatch threads.sbatch --job-name mp1_8 --cpus-per-task 8 ./mp1 8
