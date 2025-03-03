9eedaebf1810fa5fa1e972c163a2dc833aca6413
Gaurav Dadlaney <gdadlaney3@gatech.edu>
2025-03-03 02:25:30 -0500

    Add scripts & sbatch files for mp2 & mpi2

---
+++ mpi/run_experiments1.sh
@@ -0,0 +1,8 @@
+sbatch --job-name=mpi2_1 --nodes=1 nodes.sbatch ./mpi2 1
+sbatch --job-name=mpi2_2 --nodes=2 nodes.sbatch ./mpi2 2
+sbatch --job-name=mpi2_3 --nodes=3 nodes.sbatch ./mpi2 3
+sbatch --job-name=mpi2_4 --nodes=4 nodes.sbatch ./mpi2 4
+sbatch --job-name=mpi2_5 --nodes=5 nodes.sbatch ./mpi2 5
+sbatch --job-name=mpi2_6 --nodes=6 nodes.sbatch ./mpi2 6
+sbatch --job-name=mpi2_7 --nodes=7 nodes.sbatch ./mpi2 7
+sbatch --job-name=mpi2_8 --nodes=8 nodes.sbatch ./mpi2 8
---
+++ omp/run_experiments2.sh
@@ -0,0 +1,8 @@
+sbatch --job-name=mp2_1 --cpus-per-task=1 threads.sbatch ./mp2 1
+sbatch --job-name=mp2_2 --cpus-per-task=2 threads.sbatch ./mp2 2
+sbatch --job-name=mp2_3 --cpus-per-task=3 threads.sbatch ./mp2 3
+sbatch --job-name=mp2_4 --cpus-per-task=4 threads.sbatch ./mp2 4
+sbatch --job-name=mp2_5 --cpus-per-task=5 threads.sbatch ./mp2 5
+sbatch --job-name=mp2_6 --cpus-per-task=6 threads.sbatch ./mp2 6
+sbatch --job-name=mp2_7 --cpus-per-task=7 threads.sbatch ./mp2 7
+sbatch --job-name=mp2_8 --cpus-per-task=8 threads.sbatch ./mp2 8
